b23f774e7f0e78dc326de4cf1a3c667e2ebbcd25
HengzhiChen <45375761+HengzhiChen@users.noreply.github.com>
2018-12-18 09:56:30 +0800

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,13 +1,17 @@
 
-int[] a = {8,6,7,3,0};//6,7,3,0,8
-int b = 0;
-while(b < a.length){
-    for( int i = 0;i<a.length-1;i++){
-        if(a[i]>a[i+1]){
-           int tmp = a[i];
-           a[i] = a[i+1];
-           a[i+1]=tmp;
-        }
-    }
+    int[] a = {8,6,7,3,0};//6,7,3,0,8
+    int b = 0;
+    while(b < a.length){
+        for( int i = 0;i<a.length-1;i++){
+            if(a[i]>a[i+1]){
+                int tmp = a[i];
+                a[i] = a[i+1];
+                a[i+1]=tmp;
+            }
+         }
       b++;
-}
+     }
+     for(int i = 0;i<a.length;i++){
+         System.out.print(a[i]);
+     }
+    
